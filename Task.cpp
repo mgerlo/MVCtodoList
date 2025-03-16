621367faf6d50d9ae094f14b811a98153fe7b662
mgerlotti <matteo.gerlotti@edu.unfi.it>
2025-03-16 23:38:52 +0100

    Classe Task, attributi e metodi

--- Task.cpp
+++ Task.cpp
@@ -1,1 +1,13 @@
 #include "Task.h"
+
+Task::Task(string desc, string d, string t, int p, bool c) : description(desc), date(d), time(t), priority(p),
+                                                             done(c) {}
+
+void Task::printTask() const {
+    cout << "Description: " << description << endl;
+    cout << "Date: " << date << endl;
+    cout << "Time: " << time << endl;
+    cout << "Priority: " << priority << endl;
+    cout << "Done: " << (done ? "Yes" : "No") << endl;
+}
+
--- Task.h
+++ Task.h
@@ -1,4 +1,64 @@
 #ifndef TODOLIST_TASK_H
 #define TODOLIST_TASK_H
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Task {
+public:
+    explicit Task(string desc = "Activity", string d = "today", string t = "10:00 AM", int p = 0, bool c = false);
+
+    const string &getDescription() const {
+        return description;
+    }
+
+    void setDescription(const string &desc) {
+        description = desc;
+    }
+
+    const string &getDate() const {
+        return date;
+    }
+
+    void setDate(const string &d) {
+        date = d;
+    }
+
+    const string &getTime() const {
+        return time;
+    }
+
+    void setTime(const string &t) {
+        time = t;
+    }
+
+    int getPriority() const {
+        return priority;
+    }
+
+    void setPriority(int p) {
+        priority = p;
+    }
+
+    bool isDone() const {
+        return done;
+    }
+
+    void setDone(bool c) {
+        done = c;
+    }
+
+    void printTask() const;
+
+private:
+    string description;
+    string date;
+    string time;
+    int priority;
+    bool done;
+};
+
+
 #endif //TODOLIST_TASK_H
