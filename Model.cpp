1d9753548e0c2104e50695752581c772f9f5f78d
mgerlotti <matteo.gerlotti@edu.unfi.it>
2025-03-23 23:32:19 +0100

    Classe Model (Subject concreto)

--- Model.cpp
+++ Model.cpp
@@ -1,1 +1,14 @@
 #include "Model.h"
+
+void Model::notify() {
+    for (auto observer: observers)
+        observer->update();
+}
+
+void Model::addObserver(Observer *o) {
+    observers.push_back(o);
+}
+
+void Model::removeObserver(Observer *o) {
+    observers.remove(o);
+}
--- Model.h
+++ Model.h
@@ -1,4 +1,33 @@
 #ifndef TODOLIST_MODEL_H
 #define TODOLIST_MODEL_H
 
+#include <list>
+#include "Subject.h"
+#include "TodoList.h"
+
+using namespace std;
+
+class Model : public Subject {
+public:
+    void notify() override;
+
+    void addObserver(Observer *o) override;
+
+    void removeObserver(Observer *o) override;
+
+    list<TodoList> getLists() const {
+        return lists;
+    }
+
+    void addList(const TodoList &list) {
+        lists.push_back(list);
+        notify();
+    }
+
+private:
+    list<TodoList> lists;
+    list<Observer *> observers;
+};
+
+
 #endif //TODOLIST_MODEL_H
