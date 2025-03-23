5591945c8350682e7ababaf76c0ed2cd79aed1ea
mgerlotti <matteo.gerlotti@edu.unfi.it>
2025-03-23 18:25:39 +0100

    Classe TodoList, attributi e metodi

--- TodoList.cpp
+++ TodoList.cpp
@@ -1,1 +1,13 @@
 #include "TodoList.h"
+
+void TodoList::printTodoList() const {
+    if (tasks.empty())
+        cout << "ToDo List is empty !" << endl;
+    else {
+        cout << "ToDo List:" << endl;
+        for (auto &task: tasks) {
+            cout << "----------" << endl;
+            task.printTask();
+        }
+    }
+}
--- TodoList.h
+++ TodoList.h
@@ -1,4 +1,26 @@
 #ifndef TODOLIST_TODOLIST_H
 #define TODOLIST_TODOLIST_H
 
+#include <list>
+#include "Task.h"
+
+using namespace std;
+
+class TodoList {
+public:
+    list<Task> getTasks() const {
+        return tasks;
+    }
+
+    void setTask(const Task &task) {
+        tasks.push_back(task);
+    }
+
+    void printTodoList() const;
+
+private:
+    list<Task> tasks;
+};
+
+
 #endif //TODOLIST_TODOLIST_H
