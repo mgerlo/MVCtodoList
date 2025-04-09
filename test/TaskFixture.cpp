4277d714ef0e69e102c83eacdb0a13ab6607b2ca
mgerlotti <matteo.gerlotti@edu.unfi.it>
2025-04-10 00:27:20 +0200

    UNIT TESTING controllo lancio eccezioni per uso di Regex

--- test/TaskFixture.cpp
+++ test/TaskFixture.cpp
@@ -8,7 +8,7 @@ protected:
 
     void SetUp() override {
         t.setDescription("Back to the Future");
-        t.setDate("tomorrow");
+        t.setDate("31-12-2024");
         t.setTime("22:04");
         t.setPriority(3);
         t.setDone(true);
@@ -17,7 +17,7 @@ protected:
 
 TEST_F(TaskSuite, TestConstructor) {
     EXPECT_EQ("Back to the Future", t.getDescription()) << "Wrong description setter method";
-    EXPECT_EQ("tomorrow", t.getDate()) << "Wrong date setter method";
+    EXPECT_EQ("31-12-2024", t.getDate()) << "Wrong date setter method";
     EXPECT_EQ("22:04", t.getTime()) << "Wrong time setter method";
     EXPECT_EQ(3, t.getPriority()) << "Wrong priority setter method";
     EXPECT_TRUE(t.isDone()) << "Wrong done setter method";
--- test/TaskTest.cpp
+++ test/TaskTest.cpp
@@ -6,8 +6,23 @@ TEST(Task, DefaultConstructor) {
     Task t;
     EXPECT_EQ("Activity", t.getDescription());
     // EXPECT_STRCASEEQ("actIVITY", "AcTiViTry");   // trying C-strings comparison
-    EXPECT_EQ("today", t.getDate());
-    EXPECT_EQ("10:00 AM", t.getTime());
+    EXPECT_EQ("01-01-2025", t.getDate());
+    EXPECT_EQ("10:00", t.getTime());
     EXPECT_EQ(0, t.getPriority()) << "Default Task has priority != 0";
     EXPECT_FALSE(t.isDone()) << "Default Task should NOT be DONE";
+}
+
+// Test formati validi data e orario
+TEST(Task, ValidConstructor) {
+    EXPECT_NO_THROW(Task("Prova", "01-01-2020", "16:00"));
+}
+
+// Test formato data non valido
+TEST(Task, InvalidDate) {
+    EXPECT_THROW(Task("Errore data", "2025-01-01"), invalid_argument);
+}
+
+// Test formato orario non valido
+TEST(Task, InvalidTime) {
+    EXPECT_THROW(Task("Errore orario", "01-01-2025", "Paperino", 1, false), invalid_argument);
 }
